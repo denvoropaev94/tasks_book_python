dfca9cf895ffed64322364c23c3a5fedc3a70c2b
«Denis» <denvoropaev7@gmail.com>
2023-07-11 14:34:15 +0300

    taski oop

---
+++ oop/eq_hash.py
@@ -0,0 +1,42 @@
+from math import sqrt
+
+
+class Triangle:
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def __str__(self):
+        return f'Треугольник со сторонами: {self.a}, {self.b}, {self.c}'
+
+    def __repr__(self):
+        return f'Triangle({self.a}, {self.b}, {self.c})'
+
+    def __eq__(self, other):
+        first = sorted((self.a, self.b, self.c))
+        second = sorted((other.a, other.b, other.c))
+        return first == second
+
+    def area(self):
+        p = (self.a + self.b + self.c) / 2
+        _area = sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
+        return _area
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __hash__(self):
+        return hash((self.a, self.b, self.c))
+
+
+# one = Triangle(3, 4, 5)
+# two = Triangle(5, 5, 5)
+# print(f'{one} имеет площадь {one.area():.3f} y.e.2')
+# print(f'{two} имеет площадь {two.area():.3f} y.e.2')
+# print(f'{one > two = }\n{one < two = }')
+
+tr_set = {Triangle(3, 4, 5), Triangle(6, 2, 5), Triangle(4, 4, 4), Triangle(8, 5, 5)}
+print(tr_set)
+print(', '.join(f'{hash(item)}' for item in tr_set))

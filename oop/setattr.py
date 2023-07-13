208ad1677e892d0aa84965c80e9abdc363a3c2be
«Denis» <denvoropaev7@gmail.com>
2023-07-13 12:19:11 +0300

    setattr

---
+++ oop/setattr.py
@@ -0,0 +1,20 @@
+class Alpha:
+
+    def __setattr__(self, key, value):
+        if key == "number" and type(value) != int:
+            res = 0
+        else:
+            res = value
+        self.__dict__[key] = res
+
+A = Alpha()
+A.value = "Object A"
+print(A.value)
+A.number = 777
+print(A.number)
+A.number = "Hello"
+print(A.number)
+A.value = 3213231
+print(A.value)
+A.__dict__["number"] = "Python"
+print(A.number)
